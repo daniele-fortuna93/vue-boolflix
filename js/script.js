5c4fbbd679fa9c1f9642dbca5b0a08b12e2cdafe
Daniele <bambudio@libero.it>
2021-04-09 16:22:07 +0200

    Aggiunto cambio lingua

--- js/script.js
+++ js/script.js
@@ -2,27 +2,46 @@ var app = new Vue(
   {
     el: '#root',
     data:{
+      movieHome:[],
       name: '',
       urlImg: 'https://image.tmdb.org/t/p/w500',
-      searchMovie:[]
+      searchMovie:[],
+      lang: 'it-IT'
+    },
+    mounted: function(){
+      const self = this;
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params:{
+          api_key: '1824bf509354c7052f4a42663578bec1',
+          query: 'ritorno al futuro',
+          language: 'it-IT'
+        }
+      }).then(function (response){
+        for (var i = 0; i < response.data.results.length; i++) {
+          self.movieHome.push(response.data.results[i]);
+          self.movieHome[i].vote_average = Math.ceil(self.movieHome[i].vote_average / 2);
+        }
+      })
     },
     methods:{
       search: function() {
         const self = this;
         self.searchMovie = [];
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-          params:{
-            api_key: '1824bf509354c7052f4a42663578bec1',
-            query: self.name,
-            language: 'it-IT'
-          }
-        }).then(function (response){
-          for (var i = 0; i < response.data.results.length; i++) {
-            self.searchMovie.push(response.data.results[i]);
-            self.searchMovie[i].vote_average = Math.ceil(self.searchMovie[i].vote_average / 2);
+        if ( self.name != '') {
+          axios.get('https://api.themoviedb.org/3/search/movie', {
+            params:{
+              api_key: '1824bf509354c7052f4a42663578bec1',
+              query: self.name,
+              language: self.lang
+            }
+          }).then(function (response){
+            for (var i = 0; i < response.data.results.length; i++) {
+              self.searchMovie.push(response.data.results[i]);
+              self.searchMovie[i].vote_average = Math.ceil(self.searchMovie[i].vote_average / 2);
+            }
+          });
+        }
 
-          }
-        });
       }
     }
   }
